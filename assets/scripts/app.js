cf7341274d07ccf9ba35d71bd4465d1df800706f
Stefanni Kapps <stefannikapps29@gmail.com>
2024-06-20 18:27:34 -0300

    efeitos

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -56,3 +56,21 @@ typeWritterEffect(FIRST_TITLE, TEXT);
 
 
 
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+
+      if (targetElement) {
+          const offset = -100;
+          const targetPosition = targetElement.getBoundingClientRect().top + window.scrollY + offset;
+
+          window.scrollTo({
+              top: targetPosition,
+              behavior: 'smooth'
+          });
+      }
+  });
+});
